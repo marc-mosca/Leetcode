4a6e682dc1c02b867d74349bd2b6c4b7b6529d23
Marc MOSCA <marc.mosca@icloud.com>
2024-11-05 10:39:37 +0100

    refactor: fixed space indentation

--- 3163 - String Compression III/string-compression.ts
+++ 3163 - String Compression III/string-compression.ts
@@ -6,19 +6,19 @@
 //
 
 function compressedString(word: string): string {
-    let comp: string = "";
-    let counter: number = 1;
+  let comp: string = "";
+  let counter: number = 1;
 
-    for (let i: number = 0; i < word.length; i++) {
-        if (counter < 9 && word[i] === word[i + 1]) {
-            counter += 1;
-        }
-        else {
-            comp += String(counter)
-            comp += word[i]
-            counter = 1;
-        }
+  for (let i: number = 0; i < word.length; i++) {
+    if (counter < 9 && word[i] === word[i + 1]) {
+      counter += 1;
     }
+    else {
+      comp += String(counter)
+      comp += word[i]
+      counter = 1;
+    }
+  }
 
-    return comp;
+  return comp;
 }
